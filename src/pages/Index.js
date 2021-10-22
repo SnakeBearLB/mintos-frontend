2f1ba9f28aa7a3be07b25213e5bc4719eabdbf3a
SnakeBearLB <79704111+SnakeBearLB@users.noreply.github.com>
2021-10-22 15:50:58 -0400

    upload asset

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,13 +4,31 @@ import { Link } from "react-router-dom"
 
 function Index(props) {
 
+  const [ newForm, setNewForm ] = useState({
+    title: "",
+    image: ""
+  });
+
+  const handleChange = (event) => {
+    setNewForm({...newForm, [event.target.name]: event.target.value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createAssets(newForm);
+    setNewForm({
+      title: "",
+      image: "",
+    })
+  }
+
   // loaded function
   const loaded = () => {
   return props.assets.map((asset) => (
     <div key={asset._id} className="person">
       <Link to={`/people/${asset._id}`}><h1>{asset.name}</h1></Link>
-      <img src={asset.image} alt={asset.title} />
       <h3>{asset.title}</h3>
+      <img src={asset.image} alt={asset.title} />
     </div>
   ))
   }
@@ -19,8 +37,29 @@ function Index(props) {
     return <h1>Loading...</h1>
   };
 
-  return props.assets ? loaded() : loading();
-
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newForm.title}
+          name="title"
+          placeholder="title of asset"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.image}
+          name="image"
+          placeholder="asset URL"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Upload Asset" />
+        {props.assets ? loaded() : loading()}
+        
+      </form>
+    </section>
+  )
 } 
 
 export default Index;
